757ec0b3b30425865793b66e7696a7ac6ad0056e
Guillermo Montero <guilleandmont@gmail.com>
2021-01-31 16:59:27 -0400

    This time we added .gitignore

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
-console.log('Git is so cool!')
+// First we added the files to the staging area with $ git add .
+console.log('Git is so cool!'); 
+ // After a change is made, we need to add app.js to the staging area once again.
